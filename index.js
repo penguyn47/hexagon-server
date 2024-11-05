baf2e7d0607d2d307b8f7074ff5e6049ce826529
MrPen <lan474444@gmail.com>
2024-11-05 19:54:16 +0700

    Added cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const userRoute = require('./routes/user');
@@ -7,6 +8,7 @@ const productRoute = require('./routes/product');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Routes
 
